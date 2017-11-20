26dfa927705c052bf394e6796c3b0abec0574942
nicosebastian <nszess@gmail.com>
2017-11-20 16:16:14 -0500

    added basic redux to always check auth, shows login/logout button respectivly

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -2,6 +2,9 @@
 
 import React, { Component } from "react";
 import { BrowserRouter, Route } from "react-router-dom";
+import { connect } from "react-redux";
+import * as actions from '../actions'
+
 import 'materialize-css/dist/css/materialize.min.css'; //unessecery to assign to var
 
 import Header from "./Header";
@@ -11,6 +14,11 @@ const Dashboard = () => <h2>Dashboard</h2>;
 const NewSurvey = () => <h2>NewSurvey</h2>;
 
 class App extends Component {
+  componentDidMount() {
+    //sometimes componentWillMount is called several times, so convention is to put it here
+    this.props.fetchUser();
+  }
+
   render() {
     //react router expects atmost 1 child, so need to wrap in div
     return (
@@ -29,4 +37,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default connect(null, actions)(App);
--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,21 +1,49 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
 
 class Header extends Component {
+  //helper meathod to choose if login nav should be shown
+  renderContent() {
+    switch (this.props.auth) {
+      case null:
+        //waiting for request to be resolved
+        return;
+
+      case false:
+        //logged out
+        return (
+          <li>
+            <a href="/auth/google">Login With Google</a>
+          </li>
+        );
+
+      default:
+        //logged in
+        return (
+          <li>
+            <a href="/api/logout">Logout</a>
+          </li>
+        );
+    }
+  }
+
   render() {
     //react router expects atmost 1 child, so need to wrap in div
     return (
       <nav>
         <div className="nav-wrapper">
-          <a href="/" className="left brand-logo">Emaily</a>
-          <ul className="right">
-            <li>
-              <a href="/auth/google">Login With Google</a>
-            </li>
-          </ul>
+          <a href="/" className="left brand-logo">
+            Emaily
+          </a>
+          <ul className="right">{this.renderContent()}</ul>
         </div>
       </nav>
     );
   }
 }
 
-export default Header;
+function mapStateToProps({ auth }) {
+  return { auth };
+}
+
+export default connect(mapStateToProps)(Header);
