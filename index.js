53bdfd615236a2d3373aeafda24c6ceb05ad19ef
nicosebastian <nszess@gmail.com>
2017-11-25 12:25:55 -0500

    add heroku config script

--- index.js
+++ index.js
@@ -30,5 +30,17 @@ app.get("/", (req, res) => {
 require("./routes/authRoutes")(app); //idk if i like this, require returns module.exports. in this case a function
 require("./routes/billingRoutes")(app);
 
+if (process.env.NODE_ENV == "production") {
+  //express serves production assets
+  app.use(express.static("client/build"));//see if some local file matches request uri
+
+  //otherwise serve index.html
+  const path = require('path');
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+  })
+  //express serves up index.html file if no route is recognised
+}
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
